b49ae37827d60cb3ba03aee03fff473956091f0c
Anjan Paudel <anjan.paudel@studiomatrix.xyz>
2022-06-30 16:08:29 +0545

    validators bug fixed

--- src/app/auth/component/login/login.component.ts
+++ src/app/auth/component/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/core/services/token.service';
 import { LoginService } from '../../service/login.service';
@@ -10,33 +15,40 @@ import { LoginService } from '../../service/login.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  // unamepattern = '^[a-z0-9]{8-15}$';
+  // pwdPattern = '^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?!.*s).{6,12}$';
+
   loginForm: FormGroup;
   errorMessage: '' | undefined;
   token: string = '';
+  isValidFormSubmitted = false;
   constructor(
     private loginService: LoginService,
     private router: Router,
     private formbuilder: FormBuilder
   ) {
     this.loginForm = this.formbuilder.group({
-      username: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.pattern(/[a-z]$/)),
       password: new FormControl('', Validators.required),
     });
   }
 
   ngOnInit(): void {}
   save() {
+    this.isValidFormSubmitted = false;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.isValidFormSubmitted = true;
     this.loginService.validateUser(this.loginForm.value).subscribe({
       next: (res) => {
         this.token = res.token;
         TokenService.setAccessToken(this.token);
         console.log('bhayo');
 
-      //   this.router.navigateByUrl('/post/index');
+        //   this.router.navigateByUrl('/post/index');
       },
-      error: (res) => this.errorMessage = res.error.message,
-
-
+      error: (res) => (this.errorMessage = res.error.message),
     });
   }
 }
